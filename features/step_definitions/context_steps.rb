6ca942260b5d15564a789daf043ff80e7657ebd4
Reinier Balt <lrbalt@gmail.com>
2010-10-22 20:46:06 +0200

    migrate creating a new context and refactor some project js to share with context js

--- features/step_definitions/context_steps.rb
+++ features/step_definitions/context_steps.rb
@@ -33,6 +33,7 @@ Given /^I have the following contexts$/ do |table|
   end
 end
 
+# TODO: refactor this to paths.rb
 When /^I visit the context page for "([^\"]*)"$/ do |context_name|
   context = @current_user.contexts.find_by_name(context_name)
   context.should_not be_nil
@@ -41,48 +42,8 @@ end
 
 When /^I edit the context name in place to be "([^\"]*)"$/ do |new_context_name|
   selenium.click "context_name"
-  fill_in "value", :with => "OutAndAbout"
+  fill_in "value", :with => new_context_name
   click_button "OK"
-end
-
-When /^I delete the context "([^\"]*)"$/ do |context_name|
-  context = @current_user.contexts.find_by_name(context_name)
-  context.should_not be_nil
-  click_link "delete_context_#{context.id}"
-  selenium.get_confirmation.should == "Are you sure that you want to delete the context '#{context_name}'? Be aware that this will also delete all actions in this context!"
-  wait_for do
-    !selenium.is_element_present("delete_context_#{context.id}")
-  end
-end
-
-When /^I edit the context to rename it to "([^\"]*)"$/ do |new_name|
-  click_link "edit_context_#{@context.id}"
-
-  wait_for do
-    selenium.is_element_present("submit_context_#{@context.id}")
-  end
-
-  fill_in "context_name", :with => new_name
-
-  selenium.click "submit_context_#{@context.id}",
-    :wait_for => :text,
-    :element => "flash",
-    :text => "Context saved"
-  
-  wait_for do
-    selenium.is_element_present("edit_context_#{@context.id}")
-  end
-end
-
-When /^I add a new context "([^"]*)"$/ do |context_name|
-  fill_in "context[name]", :with => context_name
-  submit_new_context_form
-end
-
-When /^I add a new hidden context "([^"]*)"$/ do |context_name|
-  fill_in "context[name]", :with => context_name
-  check "context_hide"
-  submit_new_context_form
 end
 
 Then /^I should see the context name is "([^\"]*)"$/ do |context_name|
@@ -95,9 +56,4 @@ end
 
 Then /^he should see that a context named "([^\"]*)" is not present$/ do |context_name|
   Then "I should not see \"#{context_name} (\""
-end
-
-Then /^I should see the context "([^"]*)" under "([^"]*)"$/ do |context_name, state|
-  context = Context.find_by_name(context_name)
-  response.should have_xpath("//div[@id='list-contexts-#{state}']//div[@id='context_#{context.id}']")
 end
